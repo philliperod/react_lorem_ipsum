6d626786bdcdf85584cc9025c5336989a6078bef
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-18 18:45:15 -0600

    created two hooks: one for the counter with a default value of zero; one for the generated text with a default empty array; the handleSubmit function accepts will update the count state value (change it from a string to a number), check if the count is either zero or less and set it to one, check if the count is above eight and set it to eight; will use the state function setText to access the 'data.js' array and return the values from minimally 1 to max 8 based on the state value count

--- src/App.js
+++ src/App.js
@@ -2,7 +2,44 @@ import React, { useState } from 'react';
 import data from './data';
 
 function App() {
-  return <h2>Lorem Ipsum Project Setup</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraph</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
